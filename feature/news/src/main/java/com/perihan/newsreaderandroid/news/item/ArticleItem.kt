8a10ac4a963c57299e009c6092a524316ebb46ba
perihanimamogluerol <perihanimamoglu1@gmail.com>
2025-03-10 22:07:11 +0100

    added placeholder_image

--- feature/news/src/main/java/com/perihan/newsreaderandroid/news/item/ArticleItem.kt
+++ feature/news/src/main/java/com/perihan/newsreaderandroid/news/item/ArticleItem.kt
@@ -28,13 +28,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.perihan.newsreaderandroid.domain.model.ArticleDomainModel
 import com.perihan.newsreaderandroid.core.navigation.NewsNavKeys
 import com.perihan.newsreaderandroid.core.navigation.NewsNavRoute
+import com.perihan.newsreaderandroid.domain.model.ArticleDomainModel
+import com.perihan.newsreaderandroid.news.R
 
 @Composable
 fun ArticleItem(
@@ -59,10 +61,13 @@ fun ArticleItem(
                     model = article.imageUrl,
                     contentScale = ContentScale.Crop,
                     contentDescription = "",
+                    error = painterResource(id = R.drawable.placeholder_image),
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(200.dp)
-                        .clip(RoundedCornerShape(2.dp))
+                        .clip(
+                            RoundedCornerShape(2.dp)
+                        )
                 )
 
                 IconButton(
